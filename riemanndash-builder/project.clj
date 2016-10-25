9b4b42067f94178b7635706a2b954aa9437569d1
Michael Stephan <michael.stephan@sap.com>
2016-10-25 17:47:43 +0200

    update

--- riemanndash-builder/project.clj
+++ riemanndash-builder/project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]
+                 [slingshot "0.12.2"]
+                 [com.taoensso/timbre "4.7.4"]])
